f8d7336579578236ca1c92fea1cdc8c858857fb3
Viktor Shubert <viktorshubert02@gmail.com>
2024-05-14 12:59:53 +0200

    change dim uniform to ivec3, inTexture to bind to image instead of sampler

--- WaveSimulation/src/simulation/shaders/timeStepCompute.cpp
+++ WaveSimulation/src/simulation/shaders/timeStepCompute.cpp
@@ -34,18 +34,21 @@ TimeStepCompute::TimeStepCompute(const std::string path, int dimX, int dimZ, int
 		delete[] message;
 	}
 
+	float borderColor[] = { 0.5f, 0.0f, 0.0f, 0.0f };
 	glCreateTextures(GL_TEXTURE_3D, 1, &m_inTexture);
-	glTextureParameteri(m_inTexture, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTextureParameteri(m_inTexture, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTextureParameteri(m_inTexture, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+	glTextureParameteri(m_inTexture, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTextureParameteri(m_inTexture, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTextureParameterfv(m_inTexture, GL_TEXTURE_BORDER_COLOR, borderColor);
+	glTextureParameteri(m_inTexture, GL_TEXTURE_WRAP_R, GL_REPEAT);
 	glTextureParameteri(m_inTexture, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTextureParameteri(m_inTexture, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTextureStorage3D(m_inTexture, 1, GL_R32F, dimX, dimZ, dimTheta);
 
 	glCreateTextures(GL_TEXTURE_3D, 1, &m_outTexture);
-	glTextureParameteri(m_outTexture, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTextureParameteri(m_outTexture, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTextureParameteri(m_outTexture, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+	glTextureParameteri(m_outTexture, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTextureParameteri(m_outTexture, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTextureParameterfv(m_outTexture, GL_TEXTURE_BORDER_COLOR, borderColor);
+	glTextureParameteri(m_outTexture, GL_TEXTURE_WRAP_R, GL_REPEAT);
 	glTextureParameteri(m_outTexture, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTextureParameteri(m_outTexture, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTextureStorage3D(m_outTexture, 1, GL_R32F, dimX, dimZ, dimTheta);
@@ -55,24 +58,30 @@ TimeStepCompute::~TimeStepCompute()
 {
 }
 
-void TimeStepCompute::dispatch() const
+void TimeStepCompute::dispatch()
 {
 	bind();
 	setInteger("in_Grid", 1);
-	glBindTextureUnit(1, m_inTexture);
+	//glBindTextureUnit(1, m_inTexture);
+	glBindImageTexture(1, m_inTexture, 0, GL_FALSE, 0, GL_READ_ONLY, GL_R32F);
 	setInteger("out_Grid", 2);
 	//glBindTextureUnit(2, m_outTexture);
 	glBindImageTexture(2, m_outTexture, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_R32F);
 
 	glDispatchCompute(m_dim.x, m_dim.y, m_dim.z);
+	glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
 	unbind();
+
+	GLuint temp = m_outTexture;
+	m_outTexture = m_inTexture;
+	m_inTexture = temp;
 }
 
-void TimeStepCompute::loadUniforms(glm::vec3 min, glm::vec3 delta, float groupSpeed, float dt)
+void TimeStepCompute::loadUniforms(glm::ivec3 dim, glm::vec3 min, glm::vec3 delta, float groupSpeed, float dt)
 {
 	bind();
-	glm::vec3 dim3(m_dim);
-	setVec3("u_dim", dim3);
+	//glm::vec3 dim3(m_dim);
+	setIVec3("u_dim", dim);
 	setVec3("u_min", min);
 	setVec3("u_delta", delta);
 	setFloat("u_groupSpeed", groupSpeed);
--- WaveSimulation/src/simulation/shaders/timeStepCompute.h
+++ WaveSimulation/src/simulation/shaders/timeStepCompute.h
@@ -10,8 +10,11 @@ public:
 	TimeStepCompute(const std::string path, int dimX, int dimZ, int dimTheta, int dimK);
 	~TimeStepCompute();
 
-	void dispatch() const;
-	void loadUniforms(glm::vec3 min, glm::vec3 delta, float groupSpeed, float dt);
+	void dispatch();
+	void loadUniforms(glm::ivec3 dim, glm::vec3 min, glm::vec3 delta, float groupSpeed, float dt);
+
+	GLuint getInTexture() { return m_inTexture; }
+	GLuint getOutTexture() { return m_outTexture; }
 
 private:
 
