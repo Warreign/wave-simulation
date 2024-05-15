1c6b5d6f128341b0a489f0de6db31018eb9fabc8
Viktor Shubert <viktorshubert02@gmail.com>
2024-05-15 20:08:11 +0200

    use grid setters in gui

--- WaveSimulation/src/application/components/simulationComponent.cpp
+++ WaveSimulation/src/application/components/simulationComponent.cpp
@@ -80,13 +80,13 @@ void SimulationComponent::onRenderGui()
 	ImGui::SliderFloat("Time Multiplier", &m_timeMultiplier, -2.0f, 2.0f);
 	ImGui::Separator();
 
-	if (ImGui::SliderInt("Direction", &m_simGrid->defaultDirection, 0, 15))
+	if (ImGui::SliderInt("Direction", &m_defaultDirection, 0, 15))
 	{
-		m_simGrid->set
+		m_simGrid->setDirection(m_defaultDirection);
 	}
-	if (ImGui::SliderFloat("Amplitude", &m_simGrid->defaultAmplitudeVal, 0.0f, 2.0f))
+	if (ImGui::SliderFloat("Amplitude", &m_defaultAmplitude, 0.0f, 2.0f))
 	{
-		m_simGrid->getCompute().setFloat("u_defAmplitude", m_simGrid->defaultAmplitudeVal);
+		m_simGrid->setDefaultAmp(m_defaultAmplitude);
 	}
 	
 	ImGui::SliderFloat("Wind Speed", &m_simGrid->windSpeed, 0.5, 10);
