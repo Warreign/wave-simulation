0371333a978b03c5558042cfd5916811471040b9
Viktor Shubert <viktorshubert02@gmail.com>
2024-05-15 21:32:37 +0200

    Set size and scale unis in code

--- WaveSimulation/src/application/components/visualizationComponent.cpp
+++ WaveSimulation/src/application/components/visualizationComponent.cpp
@@ -46,6 +46,9 @@ void VisualizationComponent::onUpdate()
 		m_waterShader->setInteger("u_Amplitude", 1);
 		glBindTextureUnit(1, m_simGrid.getAmpTexture());
 
+		m_waterShader->setInteger("u_waterSize", m_waterSize);
+		m_waterShader->setFloat("u_waterScale", m_waterScale);
+
 		m_waterMesh->updateData(m_ampMultiplier, m_simGrid);
 		m_waterMesh->setProfileBuffer(m_simGrid.m_profileBuffers[0]);
 	}
