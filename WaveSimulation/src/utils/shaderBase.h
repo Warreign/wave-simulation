48bcc8b3b64b3b37ba3fffc84548e05dfaee0159
Viktor Shubert <viktorshubert02@gmail.com>
2024-05-18 16:59:00 +0200

    Put compileShader() definiton outside of the class declaration

--- WaveSimulation/src/utils/shaderBase.h
+++ WaveSimulation/src/utils/shaderBase.h
@@ -46,65 +46,7 @@ public:
 protected:
 	static const std::string readShaderFromFile(const std::string& path);
 	template<class... Files>
-	static const GLuint compileShader(GLenum type, const std::string& path, const Files&... files)
-	{
-		GLint result;
-		GLuint shader = glCreateShader(type);
-
-		bool includes = false;
-		([&] {
-			includes = true;
-			const std::string& fpath = files;
-			std::string content = readShaderFromFile(fpath);
-
-			std::regex macro_regex(".*macros.*", std::regex::extended);
-			if (std::regex_search(fpath, macro_regex))
-			{
-				std::stringstream strstrm(content);
-				std::string line;
-				std::vector<std::string> lines;
-				while (std::getline(strstrm, line, '\n'))
-					lines.push_back(line);
-				lines.pop_back();
-				lines.pop_back();
-				std::string nodef = "";
-				for (auto l : lines)
-					nodef += l + '\n';
-
-				content = nodef;
-				content += std::format("#define N_THETA {}\n", N_THETA);
-				content += std::format("#define N_K {}\n", N_K);
-			}
-
-			std::string glpath = "/" + fpath;
-			glNamedStringARB(GL_SHADER_INCLUDE_ARB, -1, glpath.c_str(), -1, content.c_str());
-			} (), ...);
-
-		std::string shaderSource = readShaderFromFile(path);
-		const GLchar* source = shaderSource.c_str();
-		glShaderSource(shader, 1, &source, nullptr);
-		const char* search_directories[] = { "/", "/shaders" };
-		glCompileShaderIncludeARB(shader, _countof(search_directories), search_directories, NULL);
-
-		glGetShaderiv(shader, GL_COMPILE_STATUS, &result);
-		if (result == GL_FALSE)
-		{
-			GLint logSize;
-			glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logSize);
-
-			GLchar* message = new GLchar[logSize];
-			glGetShaderInfoLog(shader, logSize, &logSize, message);
-
-			std::cerr << "ERROR: Shader " << path << " failed to compile: " << std::endl;
-			std::cerr << message << std::endl;
-
-			glDeleteShader(shader);
-			delete[] message;
-			return 0;
-		}
-
-		return shader;
-	}
+	static const GLuint compileShader(GLenum type, const std::string& path, const Files&... files);
 
 	GLint uniformLocation(const std::string& name);
 	GLint attribLocation(const std::string& name);
@@ -113,3 +55,64 @@ protected:
 
 
 };
+
+template<class ...Files>
+inline const GLuint ShaderBase::compileShader(GLenum type, const std::string& path, const Files & ...files)
+{
+	GLint result;
+	GLuint shader = glCreateShader(type);
+
+	bool includes = false;
+	([&] {
+		includes = true;
+		const std::string& fpath = files;
+		std::string content = readShaderFromFile(fpath);
+
+		std::regex macro_regex(".*macros.*", std::regex::extended);
+		if (std::regex_search(fpath, macro_regex))
+		{
+			std::stringstream strstrm(content);
+			std::string line;
+			std::vector<std::string> lines;
+			while (std::getline(strstrm, line, '\n'))
+				lines.push_back(line);
+			lines.pop_back();
+			lines.pop_back();
+			std::string nodef = "";
+			for (auto l : lines)
+				nodef += l + '\n';
+
+			content = nodef;
+			content += std::format("#define N_THETA {}\n", N_THETA);
+			content += std::format("#define N_K {}\n", N_K);
+		}
+
+		std::string glpath = "/" + fpath;
+		glNamedStringARB(GL_SHADER_INCLUDE_ARB, -1, glpath.c_str(), -1, content.c_str());
+		} (), ...);
+
+	std::string shaderSource = readShaderFromFile(path);
+	const GLchar* source = shaderSource.c_str();
+	glShaderSource(shader, 1, &source, nullptr);
+	const char* search_directories[] = { "/", "/shaders" };
+	glCompileShaderIncludeARB(shader, _countof(search_directories), search_directories, NULL);
+
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE)
+	{
+		GLint logSize;
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logSize);
+
+		GLchar* message = new GLchar[logSize];
+		glGetShaderInfoLog(shader, logSize, &logSize, message);
+
+		std::cerr << "ERROR: Shader " << path << " failed to compile: " << std::endl;
+		std::cerr << message << std::endl;
+
+		glDeleteShader(shader);
+		delete[] message;
+		return 0;
+	}
+
+	return shader;
+}
