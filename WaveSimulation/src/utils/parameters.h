8ba4042c550128bfb31cda3498d77dfaa8b7e10b
Viktor Shubert <viktorshubert02@gmail.com>
2024-05-18 14:33:26 +0200

    add assertions for the grid size

--- WaveSimulation/src/utils/parameters.h
+++ WaveSimulation/src/utils/parameters.h
@@ -3,3 +3,6 @@
 #define N_SPATIAL 128 // number of spatial disc. nodes
 #define N_THETA 16	  // number of wave angle disc. nodes
 #define N_K 1		  // number of wave length disc. nodes
+
+static_assert(N_SPATIAL % 16 == 0); // Assert that spatial dim is divisible by 16
+static_assert(N_THETA % 4 == 0); // Assert that number of directions is divisible by 4
