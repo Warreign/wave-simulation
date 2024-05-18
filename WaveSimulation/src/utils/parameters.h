60f04c0e6f9c828524c6d563cf878c8ded3007f5
Viktor Shubert <viktorshubert02@gmail.com>
2024-05-18 14:36:57 +0200

    add minimum size assertions

--- WaveSimulation/src/utils/parameters.h
+++ WaveSimulation/src/utils/parameters.h
@@ -4,5 +4,5 @@
 #define N_THETA 16	  // number of wave angle disc. nodes
 #define N_K 1		  // number of wave length disc. nodes
 
-static_assert(N_SPATIAL % 16 == 0); // Assert that spatial dim is divisible by 16
-static_assert(N_THETA % 4 == 0); // Assert that number of directions is divisible by 4
+static_assert(N_SPATIAL % 16 == 0 && N_SPATIAL >= 16); // Assert that spatial dim is divisible by 16
+static_assert(N_THETA % 4 == 0 && N_THETA >= 4); // Assert that number of directions is divisible by 4
