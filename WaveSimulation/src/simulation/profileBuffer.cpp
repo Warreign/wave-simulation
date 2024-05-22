ef1cb98a809a8719e01bb13855c5a87eb7ae9648
Viktor Shubert <viktorshubert02@gmail.com>
2024-05-23 00:56:46 +0200

    clean profile buffer

--- WaveSimulation/src/simulation/profileBuffer.cpp
+++ WaveSimulation/src/simulation/profileBuffer.cpp
@@ -18,41 +18,6 @@ ProfileBuffer::~ProfileBuffer()
     glDeleteTextures(1, &m_texture);
 }
 
-void ProfileBuffer::precompute(std::function<float(float,float)> spectrum, float windSpeed, float kmin, float kmax,float time, int size)
-{
-    values.resize(m_resolution);
-    period = kmax * 2;
-#ifdef NDEBUG
-#pragma omp parallel for
-#endif
-    for (int i = 0; i < m_resolution; i++) {
-
-        float p = (float(i * period) / m_resolution) + kmin;
-
-        values[i] = integrate(INTEGRATION_SAMPLES, kmin, kmax, [&](float k) {
-
-            float knum = TAU / k;
-            float phase1 = knum * p - dispersionRelation(knum) * time;
-            float phase2 = knum * (p + period) - dispersionRelation(knum) * time;
-
-            float c = p / period;
-
-            return k * spectrum(k, windSpeed) * (c * calculateDisplacement(phase1, knum) + (1-c) * calculateDisplacement(phase2, knum));
-
-            //return k * spectrum(k, windSpeed) *
-            //    (calculateDisplacement(phase2, knum));
-
-            //float knum = TAU / k;
-            //float omega = sqrt(9.81 * k);
-            //float phase = knum * p - omega * time;
-            //return spectrum(k, windSpeed) * cos(phase) * k * calculateDisplacement(phase, knum);
-
-            }
-        );
-
-    }
-}
-
 void ProfileBuffer::precompute(ProfileCompute& profileCompute, float windSpeed, float kmin, float kmax, float time, int periodicity, int size)
 {
     period = kmax * periodicity;
@@ -74,21 +39,5 @@ uint32_t ProfileBuffer::resolution() const
 {
     return m_resolution;
 }
-
-const void* ProfileBuffer::data() const 
-{
-    return static_cast<const void*>(values.data());
-}
-
-float ProfileBuffer::dispersionRelation(float k) const
-{
-    return sqrt(9.81 * k);
-};
-glm::vec4 ProfileBuffer::calculateDisplacement(float phase, float k) const
-{
-    float s = sin(phase);
-    float c = cos(phase);
-    return glm::vec4(-s, c, - k * c, - k * s );
-};
 
 
--- WaveSimulation/src/simulation/profileBuffer.h
+++ WaveSimulation/src/simulation/profileBuffer.h
@@ -10,10 +10,7 @@
 
 class ProfileBuffer {
 protected:
-    float dispersionRelation(float k) const;
-    glm::vec4 calculateDisplacement(float phase , float knum) const;
 
-    std::vector<glm::vec4> values;
     uint32_t m_resolution;
     GLuint m_texture;
 
@@ -23,13 +20,11 @@ public:
 
     float period;
 
-    void precompute(std::function<float(float, float)> spectrumFunc,float windSpeed, float kmin, float kmax, float time, int resolution = 4096);
     void precompute(ProfileCompute& profileCompute, float windSpeed, float kmin, float kmax, float time, int periodicity, int resolution = 4096);
 
     void precomputeVector(ProfileCompute& profileCompute, float windSpeed, float kmin, float kmax, float time, int kc, int periodicity, int resolution = 4096);
 
     GLuint getTexture() const;
     uint32_t resolution() const;
-    const void* data() const;
 };
 
